7c981f349c6a8110f3249ee417ce0f1bdf6048bf
0xD3FC5C <70857983+0xD3FC5C@users.noreply.github.com>
2020-09-20 11:24:56 +0200

    Add files via upload

---
+++ MovieStarPlanet/Project/FinalProject/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(494, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "ActorId";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(182, 223);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(218, 82);
+            this.sendBtn.TabIndex = 1;
+            this.sendBtn.Text = "Send request";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "pl",
+            "de",
+            "com",
+            "co.uk"});
+            this.comboBox1.Location = new System.Drawing.Point(41, 170);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(494, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 356);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Final project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ MovieStarPlanet/Project/FinalProject/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FluorineFx.IO;
+using System.IO;
+using System.Net;
+using DoloModeOn;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void sendBtn_Click(object sender, EventArgs e)
+        {
+            object[] arguments = { Convert.ToInt32(textBox1.Text) };
+            AMFMessage _amf = new AMFMessage(3);
+            _amf.AddHeader(new AMFHeader("id", false, Hash.createChecksum(arguments)));
+            _amf.AddHeader(new AMFHeader("needClassName", false, true));
+            _amf.AddBody(new AMFBody("MovieStarPlanet.WebService.UserSession.AMFUserSessionService.GetActorNameFromId", "/1",  arguments));
+            MemoryStream mStream = new MemoryStream();
+            AMFSerializer serialize = new AMFSerializer(mStream);
+            serialize.WriteMessage(_amf);
+            HttpClient client = new HttpClient();
+            byte[] AMFBytes = Encoding.Default.GetBytes(Encoding.Default.GetString(mStream.ToArray()));
+
+            ///Set referer to send AMF without 404 error
+            Uri referer = new Uri("https://assets.mspcdns.com/msp/91.0.2/Main_20200728_110605.swf");
+            client.DefaultRequestHeaders.Referrer = referer;
+
+            try
+            {
+                string gateway = "https://ws-" + comboBox1.Text + ".mspapis.com/msp/91.0.6/Gateway.aspx?method=MovieStarPlanet.WebService.UserSession.AMFUserSessionService.GetActorNameFromId";
+                var AMFHttpClientByteArray = new ByteArrayContent(AMFBytes);
+                AMFHttpClientByteArray.Headers.ContentType = new MediaTypeHeaderValue("application/x-amf");
+
+                await client.PostAsync(gateway, AMFHttpClientByteArray);
+            }
+
+            catch(WebException exception)
+            {
+                MessageBox.Show("Error!" + exception);
+            }
+        }
+    }
+}s
